8525517ac1ec8158ed24aa79ce9debb2a5049e81
Stephen Zhuang <stephen.zhuang@gmail.com>
2013-01-02 14:27:27 +0800

    Add another demo for long long view using xib

---
+++ BookBot/Controllers/BBXibLongViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBXibLongViewController.h
+//  BookBot
+//
+//  Created by everbird on 1/2/13.
+//  Copyright (c) 2013 Douban.com Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBXibLongViewController : UIViewController
+
+@end
